13b46a151f7b68746be657ac722a6166e2420dd2
Valeria V <54474237+lvvargas-aponte@users.noreply.github.com>
2023-08-04 22:20:16 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateExcuse();
+};
+
+let generateExcuse = () => {
+  //pieces of the sentence used to generate my excuse
+  let who = ["The cat", "His turtle", "Her bird", "The bear"];
+  let action = ["ate", "peed", "crushed", "broke", "burned", "lost"];
+  let what = ["my homework", "the keys", "the car", "the house", "my phone"];
+  let when = [
+    "before the class",
+    "right on time",
+    "at dawn",
+    "during my lunch",
+    "while I was asleep",
+    ""
+  ];
+
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomAction = Math.floor(Math.random() * action.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+  let randomWhen = Math.floor(Math.random() * when.length);
+
+  return (
+    who[randomWho] +
+    " " +
+    action[randomAction] +
+    " " +
+    what[randomWhat] +
+    " " +
+    when[randomWhen]
+  );
 };
